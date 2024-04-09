cf297a851fdff46eee4461f9b840df70c8b61b99
Gonzalo_CR <161965777+Gonzalo-CR@users.noreply.github.com>
2024-04-09 09:46:43 -0300

    Update paneldesplegable.js

--- JS/paneldesplegable.js
+++ JS/paneldesplegable.js
@@ -1,14 +1,15 @@
-document.getElementById('explorerItem').addEventListener('click', function() {
+var explorerItem = document.getElementById('explorerItem');
+var smallexplorerItem = document.getElementById('smallexplorerItem');
+var form = document.getElementById('paneldesplegable');
+
+function toggleFormDisplay(event) {
     event.preventDefault();
-    var form = document.getElementById('paneldesplegable');
     if (form.style.display === "none") {
         form.style.display = "flex";
     } else {
         form.style.display = "none";
     }
-});
+}
 
-// window.addEventListener('scroll', function() {
-//     document.getElementById('formHorizontal').style.display = "none";
-// });
-
+explorerItem.addEventListener('click', toggleFormDisplay);
+smallexplorerItem.addEventListener('click', toggleFormDisplay);
