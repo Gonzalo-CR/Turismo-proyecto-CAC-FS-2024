7896772ec371edb2c996c028d2985b8c151d80ed
Gonzalo_CR <161965777+Gonzalo-CR@users.noreply.github.com>
2024-04-09 10:02:22 -0300

    Update paneldesplegable.js

--- JS/paneldesplegable.js
+++ JS/paneldesplegable.js
@@ -2,6 +2,12 @@ var explorerItem = document.getElementById('explorerItem');
 var smallexplorerItem = document.getElementById('smallexplorerItem');
 var form = document.getElementById('paneldesplegable');
 
+if (window.innerWidth <= 480) {
+    form.style.display = "none";
+} else {
+    form.style.display = "flex";
+}
+
 function toggleFormDisplay(event) {
     event.preventDefault();
     if (form.style.display === "none") {
